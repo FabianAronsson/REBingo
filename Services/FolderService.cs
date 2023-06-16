3e19403de605af881ad0539fde2d04533bfbee1f
FabianAronsson <fabianaronsson2@gmail.com>
2023-06-16 15:25:12 +0200

    Update folder service

--- Services/FolderService.cs
+++ Services/FolderService.cs
@@ -2,14 +2,20 @@
 
 public class FolderService
 {
-    public readonly string _baseSpeedPath = "./datapacks/speed_set/";
-    public readonly string _advancements = "data/flytre/advancements/detection";
-    public readonly string _clarify = "data/flytre/functions/clarify";
-    public readonly string _detect = "data/flytre/functions/detect";
-    public readonly string _structure = "data/flytre/functions/randomize_board/structure";
-    public readonly string _all_0 = "data/flytre/functions/randomize_board/structure/all_0.mcfunction";
-    public readonly string _pick_random = "data/flytre/functions/randomize_board/structure/pick_random.mcfunction";
-    public readonly string _reset_board_colors = "data/flytre/functions/reset_board_colors.mcfunction";
-    public readonly string _add_item_objectives = "./datapacks/base_generated/data/flytre/functions/add_item_objectives.mcfunction";
-    public readonly string _predicates = "data/flytre/predicates";
+    public readonly string BaseSpeedPath = "./datapacks/speed_set/";
+    public readonly string Advancements = "data/flytre/advancements/detection";
+    public readonly string Clarify = "data/flytre/functions/clarify";
+    public readonly string Detect = "data/flytre/functions/detect";
+    public readonly string Structure = "data/flytre/functions/randomize_board/structure";
+    public readonly string All0 = "data/flytre/functions/randomize_board/structure/all_0.mcfunction";
+    public readonly string PickRandom = "data/flytre/functions/randomize_board/structure/pick_random.mcfunction";
+    public readonly string ResetBoardColors = "data/flytre/functions/reset_board_colors.mcfunction";
+    public readonly string AddItemObjectives = "./datapacks/base_generated/data/flytre/functions/add_item_objectives.mcfunction";
+    public readonly string Predicates = "data/flytre/predicates";
+
+    public void CreateFolders(string itemName)
+    {
+        Directory.CreateDirectory(BaseSpeedPath + Predicates + itemName);
+        Directory.CreateDirectory(BaseSpeedPath + Detect + itemName);
+    }
 }
