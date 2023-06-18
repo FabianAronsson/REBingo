5f17b7d00355c1d10348f98aba7170c9350eb3b4
FabianAronsson <fabianaronsson2@gmail.com>
2023-06-18 03:18:19 +0200

    Add json reader and complete functionality

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System.Text.Json;
+using REBingo.Services;
+
+Console.WriteLine("Welcome to REBingo");
+
+var json = File.ReadAllText("./items.json");
+var items = JsonSerializer.Deserialize<List<string>>(json);
+
+foreach (var item in items)
+{
+    FileService.Build(item.Split(':')[1]);
+}
+Console.WriteLine("Successfully added items");
+
+Console.ReadKey();
